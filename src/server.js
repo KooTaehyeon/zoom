dd3584b240a022414c322e39f01fa048d68ae55a
Kth <vcro12123@naver.com>
2022-07-23 16:16:38 +0900

    웹소켓 start

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 import express from 'express';
-
+import http from 'http';
+import WebSocket from 'ws';
 const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => res.render('home'));
-
+app.get('/*', (rep, res) => res.redirect('/'));
 const handleListen = () => console.log(`Listening on http://localhost:3004`);
-app.listen(3004, handleListen);
+// app.listen(3004, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({
+  server,
+});
+server.listen(3004, handleListen);
